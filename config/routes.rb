9667b3d564ed1aa27ec705c01e24a7f54b6e2c35
mumbereh <siirasonh@gmail.com>
2023-12-04 12:51:31 +0300

    Add controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'transactions/index'
+  get 'transactions/show'
+  get 'transactions/new'
+  get 'transactions/create'
+  get 'categories/index'
+  get 'categories/show'
+  get 'categories/new'
+  get 'categories/create'
+  get 'welcome/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
