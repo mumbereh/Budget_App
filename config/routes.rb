77215dff6a14e138db2275c2c48a06ac8067a01e
mumbereh <siirasonh@gmail.com>
2023-12-04 15:04:10 +0300

    modifed category controller.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,3 @@
 Rails.application.routes.draw do
-  get 'transactions/index'
-  get 'transactions/show'
-  get 'transactions/new'
-  get 'transactions/create'
-  get 'categories/index'
-  get 'categories/show'
-  get 'categories/new'
-  get 'categories/create'
-  get 'welcome/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  root 'welcome#index'
 end
