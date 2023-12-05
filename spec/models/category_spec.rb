12e6c85d8f5636c4913b20a26b26744bb9c71f48
mumbereh <siirasonh@gmail.com>
2023-12-06 02:36:38 +0300

    Modified tests

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
+require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @user = User.create(name: 'm', email: 'm@gmail.com')
+    @category = Category.new(name: 'cow', icon: 'cow.png', user: @user)
+  end
+
+  it 'is valid with valid attributes' do
+    expect(@category).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    @category.name = nil
+    expect(@category).not_to be_valid
+  end
+
+  it 'is not valid without an icon' do
+    @category.icon = nil
+    expect(@category).not_to be_valid
+  end
 end
