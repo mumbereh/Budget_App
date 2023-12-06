7112d4b7b944e5672aa35a2a5cda2a15f2feaa76
mumbereh <siirasonh@gmail.com>
2023-12-06 19:46:28 +0300

    Rectified linter errors

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class LandingPageController < ApplicationController
   def home; end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Category < ApplicationRecord
   has_many :transactions, dependent: :destroy
   attribute :total_amount, :integer
