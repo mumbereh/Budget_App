4649c50de882b849fad1fe1baf89ff081fad08b9
mumbereh <siirasonh@gmail.com>
2023-12-07 15:16:10 +0300

    Update vies

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root 'home_page#welcom'
+  root 'landing_page#splash'
 
   devise_for :users
   resources :categories, only: %i[index show new create destroy] do
