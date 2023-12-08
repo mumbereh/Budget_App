8f94e4b28f1f2df8bba55c7c043d4d212feb134a
mumbereh <siirasonh@gmail.com>
2023-12-08 15:55:13 +0300

    Adjusted test as required by the previous review

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,15 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Category, type: :model do
   before :each do
-    @user = User.new(name: 'Shafiu', email: 'shafiu@gmail.com')
-    @category = Category.new(name: 'Clothes', icon: 'shirt.png', user_id: @user.id)
+    @user = User.new(name: 'sam', email: 'sam@gmail.com')
+    @category = Category.new(name: 'cow', icon: 'cow.png', user_id: @user.id)
   end
 
   it 'name should be present' do
-    expect(@category.name).to eq('Clothes')
+    expect(@category.name).to eq('cow')
   end
 
   it 'icon shold be present' do
-    expect(@category.icon).to eq('shirt.png')
+    expect(@category.icon).to eq('cow.png')
   end
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
   before :each do
-    @transaction = Transaction.new(name: 'Clothes', amount: 50)
+    @transaction = Transaction.new(name: 'cow', amount: 50)
   end
 
   it 'name should be present' do
-    expect(@transaction.name).to eq('Clothes')
+    expect(@transaction.name).to eq('cow')
   end
 
   it 'amount should be present' do
